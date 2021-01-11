7f089402e1368fb2039d588d63a254f858d80c59
ajlacognata <73899645+ajlacognata@users.noreply.github.com>
2021-01-11 02:56:09 -0500

    Update app.js

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -24,12 +24,12 @@ var counter = 0;
 var spotifyApi = new SpotifyWebApi({
   clientId: '78e6c06078b64ca6a10035adea9995ce',
   clientSecret: '0ca99a1d41954a9ebe812efdaab20956',
-  redirectUri: 'https://researchspotifyapp-27t5c.ondigitalocean.app/callback.html'
+  redirectUri: 'https://researchspotifyapp-27t5c.ondigitalocean.app/callback'
 });
 
 var client_id = '78e6c06078b64ca6a10035adea9995ce'; // Your client id
 var client_secret = '0ca99a1d41954a9ebe812efdaab20956'; // Your secret
-var redirect_uri = 'https://researchspotifyapp-27t5c.ondigitalocean.app/callback.html'; // Your redirect uri
+var redirect_uri = 'https://researchspotifyapp-27t5c.ondigitalocean.app/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
